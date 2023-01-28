56b6650bb1663e1005cb6eacda5dbb4061312714
abuthahir-19 <abuthahirstar1@gmail.com>
2023-01-28 20:34:46 +0530

    new changes

--- src/components/CartComponent.jsx
+++ src/components/CartComponent.jsx
@@ -4,7 +4,7 @@ import Cart from "./Cart";
 const CartComponent = () => {
     const link = "#"
     return (
-        <div className="rounded-xl shadow-2xl">
+        <div className="rounded-xl shadow-2xl shadow-gray-400">
             <div className="flex justify-between">
                 <h4 className="text-xl ml-6 font-bold">Cart</h4>
                 <a href={link} className="mr-3 text-blue-500 font-semibold">View all</a>
