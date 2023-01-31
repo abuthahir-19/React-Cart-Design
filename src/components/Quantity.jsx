04f93b51de46175841daef8592826f308580b32b
abuthahir-19 <abuthahirstar1@gmail.com>
2023-01-31 22:05:40 +0530

    responsive changes made

--- src/components/Quantity.jsx
+++ src/components/Quantity.jsx
@@ -4,10 +4,10 @@ const Quantity = () => {
     const [count, setCount] = useState (1);
 
     return (
-        <div className="flex border-2 rounded-md">
-            <button className="px-4 py-2 border-r-2" onClick={() => count > 0 && setCount (prev => prev - 1)}>-</button>
+        <div className="border-2 rounded-md sm:flex">
+            <button className="px-4 py-2 border-r-2 ultra-xs:px-2 ultra-xs:py-1" onClick={() => count > 0 && setCount (prev => prev - 1)}>-</button>
             <span className="px-4 py-2">{ count }</span>
-            <button className="px-4 py-2 border-l-2" onClick={() => setCount (prev => prev + 1)}>+</button>
+            <button className="px-4 py-2 border-l-2 ultra-xs:px-2 ultra-xs:py-1" onClick={() => setCount (prev => prev + 1)}>+</button>
         </div>
     )
 }
--- src/components/Status.jsx
+++ src/components/Status.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Status = ({ content, icon, bgColor, textColor }) => {
     return (
-        <div className={`inline-block items-center px-2 py-1 w-auto ${bgColor} rounded-md`}>
+        <div className={`inline-block items-center px-2 py-1 w-auto ${bgColor} rounded-md ultra-xs:text-sm`}>
             <div className='flex items-center justify-between'>
                 <p className={`${textColor} font-normal`}>{ content }</p>
                 <img className={`h-6 w-6 ml-2 ${textColor} relative`} src={icon} alt="delivery" />
