daef131b438c84a645ecafaac76c7bdb5c7269d9
abuthahir-19 <abuthahirstar1@gmail.com>
2023-01-31 15:31:51 +0530

    tailwind config file updated

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,17 +1,20 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
-  ],
-  theme: {
-    extend: {},
-    // screens: {
-    //   'sm' : '345px',
-    //   'md' : '540px',
-    //   'lg' : '768px',
-    //   'xl' : '1280px',
-    //   '2xl' : '1536px'
-    // }
-  },
-  plugins: [],
+    content: [
+        "./src/**/*.{js,jsx,ts,tsx}",
+    ],
+    theme: {
+        extend: {},
+        screens: {
+            'ultra-xs': { 'min': '345px', 'max': '374px' },
+            'xxs': { 'min': '375px', 'max': '474px' },
+            'xs': { 'min': '475px', 'max': '639px' },
+            'sm': '640px',
+            'md': '768px',
+            'lg': '1024px',
+            'xl': '1280px',
+            '2xl': '1536px',
+        },
+    },
+    plugins: [],
 }
