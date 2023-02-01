9ef165fdc9133759f3a866dc1ffd9fbf8a53e6a2
abuthahir-19 <abuthahirstar1@gmail.com>
2023-02-01 06:13:08 +0530

    cart component

--- src/components/CartComponent.jsx
+++ src/components/CartComponent.jsx
@@ -4,7 +4,7 @@ import Cart from "./Cart";
 const CartComponent = ({ items, onDelete }) => {
     const link = "#"
     return (
-        <div className="rounded-lg shadow-[1px_1px_10px_#888888] w-2/4 lg:w-2/3">
+        <div className="rounded-lg shadow-[1px_1px_10px_#888888] min-h-[350px] w-2/4 h-auto lg:w-2/3">
             <div className="p-4">
                 <div className="flex justify-between w-11/12 mx-auto">
                     <h4 className="text-xl font-bold">Cart</h4>
@@ -19,7 +19,7 @@ const CartComponent = ({ items, onDelete }) => {
 
 const EmptyCart = () => {
     return (
-        <div className="flex justify-center items-center w-full h-full">
+        <div className="flex justify-center items-center w-full h-auto">
             <p className="text-xl font-medium text-slate-700">Your cart is empty</p>
         </div>
     )
